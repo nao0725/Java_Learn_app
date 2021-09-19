4b012ce093bc86ff992da6af2cf235b5b593d24f
nao0725 <aqua.725.times@gmail.com>
2021-09-19 09:39:24 +0900

    [add] Java Chapter4 確認問題

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Aizawa";
+		System.out.println(name);
+		
+		name = "油そば";
+		System.out.println(name);
+		
+		int num = 7;
+		System.out.println(num);
+	}
+}
