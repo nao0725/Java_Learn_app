5218f09975b81f05f43f752f26e3d91ee27aa81c
nao0725 <aqua.725.times@gmail.com>
2021-09-19 09:31:02 +0900

    [add] Java Chapter4

--- JavaWcp/src/Chapter02.java
+++ JavaWcp/src/Chapter02.java
@@ -1,4 +1,3 @@
-
 public class Chapter02 {
 	public static void main(String[] args) {
 		System.out.println("HelloWEBCAMP！");
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,25 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		System.out.println(num1);
+
+		int num2 = 30;
+		System.out.println(num2);
+	
+	        // 定数の宣言
+	        final int MIN_NUMBER = 10;
+	        System.out.println(MIN_NUMBER);
+
+	        // 再代入しようとするとエラー
+	        //MIN_NUMBER = 20;
+	}
+}
+
