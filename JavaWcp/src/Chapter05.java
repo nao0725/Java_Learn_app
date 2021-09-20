283cd1250c3abc627e433c7105accf71d27cdda0
nao0725 <aqua.725.times@gmail.com>
2021-09-20 09:02:31 +0900

    [add] Chapter05 確認問題5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,27 @@
+public class Chapter05 {
+	public static void main(String[] args) {
+		long count = 92233720368547758L;
+		System.out.println(count);
+		
+		double name = 1234567.89;
+		System.out.println(name);
+		
+		boolean a = true;
+		System.out.println(a);
+		
+		Float b = (float) 0.123;
+		System.out.println(b);
+		
+		Integer c = 1234567;
+		System.out.println(c);
+		
+		Character d = 'A';
+		System.out.println(d);
+		
+		int[] array = new int[] {1,2,3,4,5};
+		System.out.println(array[3]);
+		}
+}
+
+
+
