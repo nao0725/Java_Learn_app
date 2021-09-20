944140525bf7eeef78cecf253c8f12f38889cdc2
nao0725 <aqua.725.times@gmail.com>
2021-09-21 08:41:45 +0900

    [add] Chapter07 確認問題7 問題１

---
+++ JavaWcp/src/Chapter07.java
@@ -0,0 +1,18 @@
+
+public class Chapter07 {
+	public static void main(String[] args) {
+		int a = 1000;
+		if (a <= 5) {
+			System.out.println("とても近いです");
+		}
+		else if (a <= 10) {
+			System.out.println("近いです");
+		}
+		else if (a <= 15) {
+			System.out.println("遠いです");
+		}
+		else {
+			System.out.println("とても遠いです");
+		}
+	}
+}
