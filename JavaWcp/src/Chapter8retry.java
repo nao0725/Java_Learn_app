7e17a38ff1046a59f417d45c1df7d37b8b974b7e
nao0725 <aqua.725.times@gmail.com>
2021-09-22 08:34:34 +0900

    [add] Capter08 retry

---
+++ JavaWcp/src/Chapter8retry.java
@@ -0,0 +1,10 @@
+
+public class Chapter8retry {
+    public static void main(String[] args) {
+        int number = 1;
+        while (number < 5){
+        	System.out.println(number * number);
+        	number++;
+        } 
+    }
+}
