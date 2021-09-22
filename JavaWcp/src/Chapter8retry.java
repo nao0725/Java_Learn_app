f2696af9afbb33e6689a0a129159af5b1045d737
nao0725 <aqua.725.times@gmail.com>
2021-09-22 10:06:13 +0900

    [add] Capter09

--- JavaWcp/src/Chapter8retry.java
+++ JavaWcp/src/Chapter8retry.java
@@ -1,9 +1,13 @@
 
 public class Chapter8retry {
     public static void main(String[] args) {
+//    	変数numberを定義する
         int number = 1;
+//      「１以上５未満」なので４以下の数字を条件式に入れる
         while (number < 5){
+//        	「二乗」を表すために変数同士をかける ex 2の二乗＝ 2*2
         	System.out.println(number * number);
+//        	numberが１つずつインクリメント（増加）し、3乗、4乗と増えていく
         	number++;
         } 
     }
