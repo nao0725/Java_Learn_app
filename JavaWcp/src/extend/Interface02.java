198dc8ee535f394ecbc284fb4857a3cdf2060487
nao0725 <aqua.725.times@gmail.com>
2021-09-29 09:29:21 +0900

    [add] Java Practice

---
+++ JavaWcp/src/extend/Interface02.java
@@ -0,0 +1,11 @@
+package extend;
+
+import extend.phone.MobilePhone;
+
+public class Interface02 {
+	public static void main(String[] args) {
+		MobilePhone mobilePhone = new MobilePhone("000000-0000000000-00000000");
+		mobilePhone.powerOff();
+	}
+
+}
