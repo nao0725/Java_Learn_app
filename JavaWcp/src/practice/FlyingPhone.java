0d38e5ea61833f6069fdc7f6ebe15c86f06613ff
nao0725 <aqua.725.times@gmail.com>
2021-09-29 10:06:38 +0900

    [add] Java Chapter13

--- JavaWcp/src/practice/FlyingPhone.java
+++ JavaWcp/src/practice/FlyingPhone.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class FlyingPhone {
+import extend.phone.Phone;
 
+public class FlyingPhone implements Phone, Flying {
+    private int count;
+    
+	public FlyingPhone(int count) {
+		this.count = count;
+	}
+    
+	public void fly() {
+		System.out.println(count + "分間飛びます");
+	}
+
+	public void call(String number) {
+		System.out.println(number + "に電話します。通話できるのは飛んでいる間だけです.");
+	}
+
+	public void powerOff() {
+		Flying.super.powerOff();
+	}   
 }
